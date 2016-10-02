54e04922fab1da57d2e58ff45ade47a921a429af
VladMiryaha <vlad.miryaha@gmail.com>
2016-10-02 13:13:02 +0300

    fixit

--- infa_s03_5.c
+++ infa_s03_5.c
@@ -4,15 +4,26 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+
 #define N 10
 
 void Split(char* string, char*** tokens, int* sleepTime);
+
+/*
+ * Некоторые стараются всегда добавлять глагол в название ф-й: GetNumberOfPrograms, GetCommands ...
+ * Но в данное случае это вопрос дискуссионный и строгих правил нет.
+ * Сами для себя решите, какой вариант вам кажется более понятным (вам дали чужой год, и вам надо в нем разобраться. в каком случае вы бы потратили меньше времени).
+ */
 int NumberOfPrograms(FILE* stream);
+
+/*
+ * Старайтесь называть ф-и единообразно. Если пишете каждое слово с заглавной буквы, то делайте это везде.
+ */
 void fgetstr(FILE* stream, char** string); 
 char** Commands(FILE* stream);
 
 int main() {
-	int status;	
+	int status;
 	FILE* file = fopen("inp.txt", "r");
 	int i;	
 	int n = NumberOfPrograms(file);
@@ -27,11 +38,21 @@ int main() {
 		if (pid == 0) {
 			Split(command, &commands, &sleepTime); 
 			sleep(sleepTime);
-			execvp(commands[1], commands+1);
+			execvp(commands[1], commands + 1);
+/*
+ * FIXIT:
+ * Представьте, что произойдет, если execvp не запустится (вернет ошибку) ...
+ * Видимо, у вас дочерний процесс будет также участвовать в запуске задач. Надо как-то избежать такой ситуации.
+ */
 		}
 	}
 	fclose(file);
 
+  /*
+   * FIXIT:
+   * Память нужно освобождать. В следующем семестре мы познакомся с "умными" указателями, которые позволяют не думать
+   * про освобождение памяти, но сейчас это надо делать самим "вручную".
+   */
 	return 0;
 }
 
@@ -60,8 +81,15 @@ void Split(char* string, char*** tokens, int* sleepTime) {
 	for (j = 0; ; j++, string = NULL) {
         if ((token = strtok(string, " ")) == NULL)        
 			break;
-		*tokens = (char**) realloc(*tokens, (j+1)*sizeof(char*));
+		*tokens = (char**) realloc(*tokens, (j + 1)*sizeof(char*));
+    /*
+     * Зачем этот malloc(1) вам понадобился?
+     */
 		(*tokens)[j] = (char*) malloc(1);
+    /*
+     * FIXIT:
+     * Ставьте пробеды вокруг бинарных операторов: strlen(token) + 1
+     */
 		(*tokens)[j] = (char*) realloc((*tokens)[j], strlen(token)+1);
 		strcpy((*tokens)[j], token);
 	}
