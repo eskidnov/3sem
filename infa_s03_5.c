7afa64df35bfff1cf644c2558e91d259cdd188a4
Egor <eskidnov@gmail.com>
2016-10-10 16:25:49 +0300

    fixed 10.10.2016

--- infa_s03_5.c
+++ infa_s03_5.c
@@ -7,62 +7,47 @@
 
 #define N 10
 
-void Split(char* string, char*** tokens, int* sleepTime);
-
-/*
- * Некоторые стараются всегда добавлять глагол в название ф-й: GetNumberOfPrograms, GetCommands ...
- * Но в данное случае это вопрос дискуссионный и строгих правил нет.
- * Сами для себя решите, какой вариант вам кажется более понятным (вам дали чужой год, и вам надо в нем разобраться. в каком случае вы бы потратили меньше времени).
- */
-int NumberOfPrograms(FILE* stream);
-
-/*
- * Старайтесь называть ф-и единообразно. Если пишете каждое слово с заглавной буквы, то делайте это везде.
- */
-void fgetstr(FILE* stream, char** string); 
+int Split(char* string, char*** tokens, int* sleepTime);
+int GetNumberOfPrograms(FILE* stream);
+void Fgetstr(FILE* stream, char** string); 
 char** Commands(FILE* stream);
 
 int main() {
 	int status;
 	FILE* file = fopen("inp.txt", "r");
 	int i;	
-	int n = NumberOfPrograms(file);
+	int n = GetNumberOfPrograms(file);
 	char* command;
 	char** commands;
 	int sleepTime;
+	int size;
 	
 	fgetc(file);
 	for (i = 0; i < n; i++) {
-		fgetstr(file, &command);
+		Fgetstr(file, &command);
 		pid_t pid = fork();
 		if (pid == 0) {
 			Split(command, &commands, &sleepTime); 
 			sleep(sleepTime);
 			execvp(commands[1], commands + 1);
-/*
- * FIXIT:
- * Представьте, что произойдет, если execvp не запустится (вернет ошибку) ...
- * Видимо, у вас дочерний процесс будет также участвовать в запуске задач. Надо как-то избежать такой ситуации.
- */
+			exit(-1);
+		}
+		else {
+			wait(&status);
 		}
 	}
 	fclose(file);
-
-  /*
-   * FIXIT:
-   * Память нужно освобождать. В следующем семестре мы познакомся с "умными" указателями, которые позволяют не думать
-   * про освобождение памяти, но сейчас это надо делать самим "вручную".
-   */
+	free(command);
 	return 0;
 }
 
-int NumberOfPrograms(FILE* file) {
+int GetNumberOfPrograms(FILE* file) {
 	int n;
 	fscanf(file, "%d", &n);
 	return n;
 }
 
-void fgetstr(FILE* stream, char** string) {
+void Fgetstr(FILE* stream, char** string) {
 	int c;
 	
 	*string = (char*) malloc(1);	
@@ -73,7 +58,7 @@ void fgetstr(FILE* stream, char** string) {
 	}
 }
 
-void Split(char* string, char*** tokens, int* sleepTime) {
+int Split(char* string, char*** tokens, int* sleepTime) {
 	int j;
 	char* token;
 
@@ -82,16 +67,9 @@ void Split(char* string, char*** tokens, int* sleepTime) {
         if ((token = strtok(string, " ")) == NULL)        
 			break;
 		*tokens = (char**) realloc(*tokens, (j + 1)*sizeof(char*));
-    /*
-     * Зачем этот malloc(1) вам понадобился?
-     */
-		(*tokens)[j] = (char*) malloc(1);
-    /*
-     * FIXIT:
-     * Ставьте пробеды вокруг бинарных операторов: strlen(token) + 1
-     */
-		(*tokens)[j] = (char*) realloc((*tokens)[j], strlen(token)+1);
+		(*tokens)[j] = (char*) realloc((*tokens)[j], strlen(token) + 1);
 		strcpy((*tokens)[j], token);
 	}
 	*sleepTime = atoi((*tokens)[0]);
+	return j;
 }
