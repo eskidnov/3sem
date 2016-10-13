c1893b479a18bcd01e0108bdf4a0abad3ab666de
Egor <eskidnov@gmail.com>
2016-10-13 23:46:19 +0300

    deadline 14.10.2016

--- infa_s05_5.c
+++ infa_s05_5.c
@@ -9,100 +9,100 @@
 
 struct Segment {
     int begin;
-	int end;
-	int index;
+    int end;
+    int index;
 };
 
 int a[SIZE][SIZE], b[SIZE][SIZE], c[SIZE][SIZE];
 
 void* my_thread(void* arg) 
 {
-	struct Segment segment = *((struct Segment*)arg);
-	int i, k, j; 
-	for(i = segment.begin; i < segment.end; i++)
-		for(k = 0; k < SIZE; k++)
-			for(j = 0; j < SIZE; j++)
-				c[i][j] += a[i][k] * b[k][j];
+    struct Segment segment = *((struct Segment*)arg);
+    int i, k, j; 
+    for(i = segment.begin; i < segment.end; i++)
+        for(k = 0; k < SIZE; k++)
+            for(j = 0; j < SIZE; j++)
+                c[i][j] += a[i][k] * b[k][j];
     return NULL;
 }
 
 int main()
 {	
-	pthread_t thread_id[N];
-	int result;
-	int i, j, k;
-	FILE* file;
+    pthread_t thread_id[N];
+    int result;
+    int i, j, k;
+    FILE* file;
     struct Segment segments[N];
 
 
 
-	srand(time(NULL));
+    srand(time(NULL));
 
-	file = fopen("matrixA.txt", "w");
+    file = fopen("matrixA.txt", "w");
     for (i = 0; i < SIZE; i++) { 
-		for (j = 0; j < SIZE; j++)
-			fprintf(file,"%d ", rand() % 10);
-		fprintf(file,"\n");
-	}
-	fclose(file);
+        for (j = 0; j < SIZE; j++)
+            fprintf(file,"%d ", rand() % 10);
+        fprintf(file,"\n");
+    }
+    fclose(file);
 
-	file = fopen("matrixB.txt", "w");
+    file = fopen("matrixB.txt", "w");
     for (i = 0; i < SIZE; i++) {
-		for (j = 0; j < SIZE; j++)
-			fprintf(file,"%d ", rand() % 10);
-		fprintf(file,"\n");
-	}
-	fclose(file);
+        for (j = 0; j < SIZE; j++)
+            fprintf(file,"%d ", rand() % 10);
+        fprintf(file,"\n");
+    }
+    fclose(file);
 
 
 
     
-	for (i = 0; i < N; i++) {
-		segments[i].index = i;
-		segments[i].begin = i * SIZE / N;
-		segments[i].end = segments[i].begin + SIZE / N;
-	}
+    for (i = 0; i < N; i++) {
+        segments[i].index = i;
+        segments[i].begin = i * SIZE / N;
+        segments[i].end = segments[i].begin + SIZE / N;
+    }
 
-	file = fopen("matrixA.txt", "r");
+    file = fopen("matrixA.txt", "r");
     for (i = 0; i < SIZE; i++)
-		for (j = 0; j < SIZE; j++)
-			fscanf(file,"%d", &a[i][j]);
-	fclose(file);
+        for (j = 0; j < SIZE; j++)
+            fscanf(file,"%d", &a[i][j]);
+    fclose(file);
 	
-	file = fopen("matrixB.txt", "r");		
-	for (i = 0; i < SIZE; i++)
-		for (j = 0; j < SIZE; j++)
-			fscanf(file ,"%d", &b[i][j]);
-	fclose(file);
+    file = fopen("matrixB.txt", "r");		
+    for (i = 0; i < SIZE; i++)
+        for (j = 0; j < SIZE; j++)
+            fscanf(file ,"%d", &b[i][j]);
+    fclose(file);
 
-	clock_t begin = clock();
+    clock_t begin = clock();
     for (i = 0; i < N - 1; i++) {
-		result = pthread_create(&thread_id[i], (pthread_attr_t *)NULL, my_thread, &(segments[i]));
-		if (result) {
-			printf("Can`t create thread, returned value = %d\n" , result);
-			exit(-1);
-		}
-	}
+        result = pthread_create(&thread_id[i], (pthread_attr_t *)NULL, my_thread, &(segments[i]));
+        if (result) {
+            printf("Can`t create thread, returned value = %d\n" , result);
+            exit(-1);
+        }
+    }
 
-	for(i = segments[N - 1].begin; i < segments[N - 1].end; i++)
-		for(k = 0; k < N; k++)
-			for(j = 0; j < N; j++)
-				c[i][j] += a[i][k] * b[k][j];
-	for(i = 0; i < N; i++)
-		pthread_join(thread_id[i], (void **) NULL);
-	clock_t end = clock();
+    for(i = segments[N - 1].begin; i < segments[N - 1].end; i++)
+        for(k = 0; k < N; k++)
+            for(j = 0; j < N; j++)
+                c[i][j] += a[i][k] * b[k][j];
+    for(i = 0; i < N; i++)
+        pthread_join(thread_id[i], (void **) NULL);
+    clock_t end = clock();
 	
-	file = fopen("matrixC.txt", "w");
+    file = fopen("matrixC.txt", "w");
     for (i = 0; i < SIZE; i++) {
-		for (j = 0; j < SIZE; j++)
-			fprintf(file,"%d ", c[i][j]);
-		fprintf(file,"\n");
-	}
-	fclose(file);
+        for (j = 0; j < SIZE; j++)
+            fprintf(file,"%d ", c[i][j]);
+        fprintf(file,"\n");
+    }
+    fclose(file);
 
-	double time_spent = (double)(end - begin);
-	printf("%d time = %f\n",N, time_spent);
+    double time_spent = (double)(end - begin);
+    printf("%d time = %f\n",N, time_spent);
 	
-	return 0;
+    return 0;
 }
 
